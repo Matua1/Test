f5bbbbf3b946a5cf38a515f90260fecb8e64e18e
Jim Halafihi <jihalafihi@student.phs.school.nz>
2022-05-27 09:26:40 +1200

    Initial commit

---
+++ MyFirstPython.py
@@ -0,0 +1,14 @@
+print('Hello!')
+
+
+born = input('What year were you born?')
+born = int(born)
+age = 2025 - born
+print(age)
+
+
+
+born = input('What year were you born?')
+born = int(born)
+age = 2022 - born
+print(age)
